2307a4cd01b5e786b616da1df474a144cfe59d64
rob <roblourens@gmail.com>
2012-05-31 00:14:29 -0500

    Hide window garbage in TakeActivity

--- Face/src/com/quail/face/TakeActivity.java
+++ Face/src/com/quail/face/TakeActivity.java
@@ -1,9 +1,11 @@
 package com.quail.face;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Window;
 
 public class TakeActivity extends SherlockFragmentActivity
 {
@@ -14,6 +16,10 @@ public class TakeActivity extends SherlockFragmentActivity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.take);
 
         cameraPreview = (CameraPreview) findViewById(R.id.cameraPreview);
